92772b0981b1b9cb7872676a68bd7c4eec06d5c2
soulseen <zhu_seen@163.com>
2018-07-17 14:45:25 +0800

    clean fun

--- common/common.py
+++ common/common.py
@@ -69,60 +69,6 @@ def replaceCharEntity(htmlstr):
     return htmlstr
 
 
-
-# 最大数
-def Get_Max(list):
-    return max(list)
-
-
-# 最小数
-def Get_Min(list):
-    return min(list)
-
-
-# 极差
-def Get_Range(list):
-    return max(list) - min(list)
-
-
-# 中位数
-def Get_Median(data):
-    data = sorted(data)
-    size = len(data)
-    if size % 2 == 0:  # 判断列表长度为偶数
-        median = (data[size // 2] + data[size // 2 - 1]) / 2
-    if size % 2 == 1:  # 判断列表长度为奇数
-        median = data[(size - 1) // 2]
-    return median
-
-
-# 众数(返回多个众数的平均值)
-def Get_Most(list):
-    most = []
-    item_num = dict((item, list.count(item)) for item in list)
-    for k, v in item_num.items():
-        if v == max(item_num.values()):
-            most.append(k)
-    return sum(most) / len(most)
-
-
-# 获取平均数
-def Get_Average(list):
-    sum = 0
-    for item in list:
-        sum += item
-    return sum / len(list)
-
-
-# 获取方差
-def Get_Variance(list):
-    sum = 0
-    average = Get_Average(list)
-    for item in list:
-        sum += (item - average) ** 2
-    return sum / len(list)
-
-
 # 验证请求是否完整
 def params_inact(post_data, *args):
     if not isinstance(post_data, dict):
